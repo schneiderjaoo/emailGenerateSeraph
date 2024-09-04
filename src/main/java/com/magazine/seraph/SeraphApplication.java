a4a47b9b0966e9b40fb495fb42b4cbd17226cdcc
João Schneider <joao.g@systextil.com.br>
2024-09-04 19:29:28 -0300

    Inicio do projeto

---
+++ src/main/java/com/magazine/seraph/SeraphApplication.java
@@ -0,0 +1,13 @@
+package com.magazine.seraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magazine/seraph/SeraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magazine.seraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
